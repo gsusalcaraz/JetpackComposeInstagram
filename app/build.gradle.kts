79959ad2870e68fd49b8e27becb30f220afd0eda
gsus <gsus.alcaraz@gmail.com>
2023-12-13 15:11:09 +0100

    Inyección de dependencias con Dagger Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -33,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -70,8 +71,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/jalcaraz/jetpackcomposeinstagram/login/ui/domain/LoginUseCase.kt
+++ app/src/main/java/com/jalcaraz/jetpackcomposeinstagram/login/ui/domain/LoginUseCase.kt
@@ -1,11 +1,13 @@
 package com.jalcaraz.jetpackcomposeinstagram.login.ui.domain
 
 import com.jalcaraz.jetpackcomposeinstagram.login.ui.data.network.LoginRepository
+import javax.inject.Inject
 
-class LoginUseCase {
-    private val repository = LoginRepository()
+class LoginUseCase @Inject constructor(private val repository: LoginRepository){
+//    private val repository = LoginRepository()
 
     suspend operator fun invoke(user: String, password: String): Boolean {
+//        return repository.doLogin(user, password)
         return repository.doLogin(user, password)
     }
 }
